2d2301194c123006b9d2a8be9e844fc563a7e245
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-11-23 14:34:17 +0530

    selectable functionality added

--- lib/auto_size_text.dart
+++ lib/auto_size_text.dart
@@ -6,7 +6,10 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/widgets.dart';
+import 'dart:ui' as ui show BoxHeightStyle, BoxWidthStyle;
 
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart' show SelectableText;
 part 'src/auto_size_text.dart';
 part 'src/auto_size_group.dart';
 part 'src/auto_size_group_builder.dart';
